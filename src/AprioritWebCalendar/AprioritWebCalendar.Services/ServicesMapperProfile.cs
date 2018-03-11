247542c6589d71fec0dbd91466b86e35d4c09c5d
Yurii Nosach <nosach.yurko@gmail.com>
2018-03-11 12:20:32 +0200

    Set up AutoMapper, created extensions and action filter to validate a model for API.

---
+++ src/AprioritWebCalendar/AprioritWebCalendar.Services/ServicesMapperProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace AprioritWebCalendar.Services
+{
+    /// <summary>
+    /// The class includes AutoMapper's map rules
+    /// between DTO and domain models (entities).
+    /// </summary>
+    public class ServicesMapperProfile : Profile
+    {
+    }
+}
