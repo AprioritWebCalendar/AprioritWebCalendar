9c37092bebe3c9e9d4e8881fd5aa396d7596969b
Yurii Nosach <nosach.yurko@gmail.com>
2018-03-11 11:58:18 +0200

    Services: added DTO, AuthService and CustomUserValidator.

---
+++ src/AprioritWebCalendar/AprioritWebCalendar.Services/Identity/CustomUserValidator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using AprioritWebCalendar.Data.Entities;
+
+namespace AprioritWebCalendar.Services.Identity
+{
+    public class CustomUserValidator : IUserValidator<User>
+    {
+        public async Task<IdentityResult> ValidateAsync(UserManager<User> manager, User user)
+        {
+            if (await manager.FindByNameAsync(user.UserName) != null)
+            {
+                return IdentityResult.Failed(new IdentityError
+                {
+                    Code = "UserName",
+                    Description = $"UserName \"{user.UserName}\" is already taken."
+                });
+            }
+
+            if (await manager.FindByEmailAsync(user.Email) != null)
+            {
+                return IdentityResult.Failed(new IdentityError
+                {
+                    Code = "Email",
+                    Description = $"An account with Email \"{user.Email}\" already exists."
+                });
+            }
+
+            return IdentityResult.Success;
+        }
+    }
+}
---
+++ src/AprioritWebCalendar/AprioritWebCalendar.Services/Interfaces/IAuthService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using AprioritWebCalendar.Services.DTO;
+
+namespace AprioritWebCalendar.Services.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<IdentityResult> RegisterUserAsync(RegisterDTO registerDTO);
+        Task<UserDTO> FindUserByCredentialsAsync(LoginDTO loginDTO);
+    }
+}
