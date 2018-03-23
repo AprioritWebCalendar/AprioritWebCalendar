f6cf1078d53ecf331e0eb44aba89fddd3fefd065
Yurii Nosach <nosach.yurko@gmail.com>
2018-03-23 23:53:43 +0200

    Create "CoreModule" and "CalendarModule" (Angular 5). Set up dependencies (not all). Created proxy config.

---
+++ src/AprioritWebCalendar/ClientApps/CalendarModule/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { CoreModule } from '../../../CoreModule/src/app/app.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class CalendarModule { }
