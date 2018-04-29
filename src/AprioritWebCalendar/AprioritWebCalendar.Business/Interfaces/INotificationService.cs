5c26af9454992c2dfaf419fa2faff0a103dfad94
Yurii Nosach <nosach.yurko@gmail.com>
2018-04-29 22:44:30 +0300

    Implemented NotificationService; Custom methods to calculare recurrences for events; TESTS fot them.

---
+++ src/AprioritWebCalendar/AprioritWebCalendar.Business/Interfaces/INotificationService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using AprioritWebCalendar.Business.DomainModels;
+
+namespace AprioritWebCalendar.Business.Interfaces
+{
+    public interface INotificationService
+    {
+        Task<IEnumerable<EventUser>> GetEventsToNotifyAsync(DateTime dateTime);
+    }
+}
---
+++ src/AprioritWebCalendar/AprioritWebCalendar.Business/Recurrences/RecurrenceRule.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using AprioritWebCalendar.Business.DomainModels;
+using AprioritWebCalendar.Infrastructure.Enums;
+
+namespace AprioritWebCalendar.Business.Recurrences
+{
+    public class RecurrenceRule
+    {
+        private readonly Func<DateTime, DateTime> _addFunc;
+        private readonly Period _period;
+
+        public RecurrenceRule(Period period)
+        {
+            _period = period;
+            var add = period.Type == PeriodType.Custom ? period.Cycle.Value : 1;
+
+            switch (period.Type)
+            {
+                case PeriodType.Yearly:
+                    _addFunc = (d) => d.AddYears(add);
+                    break;
+
+                case PeriodType.Monthly:
+                    _addFunc = (d) => d.AddMonths(add);
+                    break;
+
+                case PeriodType.Weekly:
+                    _addFunc = (d) => d.AddDays(add * 7);
+                    break;
+
+                case PeriodType.Custom:
+                    _addFunc = (d) => d.AddDays(add);
+                    break;
+            }
+        }
+
+        public IEnumerable<DateTime> GetDates()
+        {
+            var date = _period.PeriodStart;
+
+            do
+            {
+                yield return date;
+                date = _addFunc(date);
+            }
+            while (date <= _period.PeriodEnd);
+
+            yield break;
+        }
+    }
+}
