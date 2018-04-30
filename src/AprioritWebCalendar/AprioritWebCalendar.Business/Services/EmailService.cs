9f38ae2034757f741d4b9bd1145817842849d0cd
Yurii Nosach <nosach.yurko@gmail.com>
2018-04-30 15:16:02 +0300

    Implemented EmailService.

---
+++ src/AprioritWebCalendar/AprioritWebCalendar.Business/Services/EmailService.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using MailKit.Net.Smtp;
+using MimeKit;
+using MimeKit.Text;
+using AprioritWebCalendar.Infrastructure.Options;
+using AprioritWebCalendar.Business.Interfaces;
+
+namespace AprioritWebCalendar.Business.Services
+{
+    public class EmailService : IEmailService
+    {
+        private readonly SmtpOptions _smtpOptions;
+
+        public EmailService(IOptions<SmtpOptions> smtpOptions)
+        {
+            _smtpOptions = smtpOptions.Value;
+        }
+
+        public async Task SendAsync(string email, string subject, string message)
+        {
+            var emailMessage = new MimeMessage();
+
+            emailMessage.From.Add(new MailboxAddress(_smtpOptions.Login));
+            emailMessage.To.Add(new MailboxAddress(email));
+            emailMessage.Subject = subject;
+            emailMessage.Body = new TextPart(TextFormat.Html) { Text = message };
+
+            using (var client = new SmtpClient())
+            {
+                await client.ConnectAsync(_smtpOptions.Server, _smtpOptions.Port, _smtpOptions.UseSsl);
+                await client.AuthenticateAsync(_smtpOptions.Login, _smtpOptions.Password);
+                await client.SendAsync(emailMessage);
+
+                await client.DisconnectAsync(true);
+            }
+        }
+    }
+}
---
+++ src/AprioritWebCalendar/AprioritWebCalendar.Infrastructure/Options/SmtpOptions.cs
@@ -0,0 +1,14 @@
+﻿namespace AprioritWebCalendar.Infrastructure.Options
+{
+    public class SmtpOptions
+    {
+        public string Server { get; set; }
+        public int Port { get; set; }
+        public bool UseSsl { get; set; }
+
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+        public string FromTitle { get; set; }
+    }
+}
