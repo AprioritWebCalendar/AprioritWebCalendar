fec28309263a56238eae8aa4ed8a7884b145cbdb
Yurii Nosach <nosach.yurko@gmail.com>
2018-05-12 12:41:24 +0300

    Implemented TelegramVerificationService.

--- src/AprioritWebCalendar/AprioritWebCalendar.Business/Interfaces/ITelegramVerificationService.cs
+++ src/AprioritWebCalendar/AprioritWebCalendar.Business/Interfaces/ITelegramVerificationService.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
 namespace AprioritWebCalendar.Business.Interfaces
 {
     public interface ITelegramVerificationService
     {
         Task<bool> TryVerifyAsync(int userId, string code);
+        Task<string> GetVerificationCodeAsync(int telegramId);
     }
 }
--- src/AprioritWebCalendar/AprioritWebCalendar.Business/Services/TelegramVerificationService.cs
+++ src/AprioritWebCalendar/AprioritWebCalendar.Business/Services/TelegramVerificationService.cs
@@ -1,16 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AprioritWebCalendar.Business.Identity;
 using AprioritWebCalendar.Business.Interfaces;
+using AprioritWebCalendar.Data.Interfaces;
+using AprioritWebCalendar.Data.Models;
 
 namespace AprioritWebCalendar.Business.Services
 {
     public class TelegramVerificationService : ITelegramVerificationService
     {
-        public Task<bool> TryVerifyAsync(int userId, string code)
+        private readonly IRepository<TelegramCode> _codesRepository;
+        private readonly IRandomDataProvider _randomDataProvider;
+        private readonly CustomUserManager _userManager;
+
+        public TelegramVerificationService(IRepository<TelegramCode> codesRepository, IRandomDataProvider randomDataProvider, CustomUserManager userManager)
         {
-            throw new NotImplementedException();
+            _codesRepository = codesRepository;
+            _randomDataProvider = randomDataProvider;
+            _userManager = userManager;
+        }
+
+        public async Task<bool> TryVerifyAsync(int userId, string code)
+        {
+            var telegramIdCode = (await _codesRepository.FindAllAsync(c => c.Code.Equals(code)))
+                .FirstOrDefault();
+
+            if (telegramIdCode == null)
+                return false;
+
+            await _codesRepository.RemoveAsync(telegramIdCode);
+
+            var user = await _userManager.FindByIdAsync(userId);
+            await _userManager.AssignTelegramIdAsync(user, telegramIdCode.TelegramId);
+
+            return true;
+        }
+
+        public async Task<string> GetVerificationCodeAsync(int telegramId)
+        {
+            string code = null;
+
+            do
+            {
+                code = _randomDataProvider.GetRandomBase64String(32);
+            }
+            while (await _codesRepository.AnyAsync(c => c.Code.Equals(code)));
+
+            await _codesRepository.CreateAsync(new TelegramCode
+            {
+                TelegramId = telegramId,
+                Code = code
+            });
+            await _codesRepository.SaveAsync();
+
+            return code;
         }
     }
 }
